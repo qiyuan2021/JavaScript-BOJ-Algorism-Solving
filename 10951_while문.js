803410c363fc392734b13cf40f63a96af5ca978e
qiyuan2021 <91328608+qiyuan2021@users.noreply.github.com>
2022-01-26 00:35:01 +0900

    Create 10951_while문.js

---
+++ 10951_while문.js
@@ -0,0 +1,11 @@
+
+
+let fs = require('fs');
+let input = fs.readFileSync('input.txt').toString().split('\n');
+
+let i = 0
+while( i < input.length - 1 ){
+  let [a, b] = input[i].split(' ').map(x => Number(x));
+  console.log(a + b);
+  i++
+}
