8c3c13efe19b3ee39d2c78226498c6880f0bf4ed
qiyuan2021 <91328608+qiyuan2021@users.noreply.github.com>
2022-01-29 15:26:11 +0900

    Create 2751_정렬.js

---
+++ 2751_정렬.js
@@ -0,0 +1,20 @@
+let fs = require('fs');
+let input = fs.readFileSync('./input.txt').toString().split('\n');
+
+let n = Number(input[0])
+
+let arr = [];
+
+for (let i=1; i<= n; i++) {
+  arr.push(Number(input[i]))
+}
+
+arr.sort((a,b)=> a-b); 
+
+result = ""; 
+
+for (let i=0; i < n; i++) {
+  result += arr[i] + "\n" ;
+}
+
+console.log(result);
